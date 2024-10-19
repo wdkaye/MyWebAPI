d222d6aa7d731616c6e81e500483f83ed190b8bd
Warren Kaye <wkaye@rds.com>
2024-10-18 17:49:41 -0700

    COMP 584: Backend Week 8
    TODO: Before week 9: fix my defect which is preventing countries being output to SwaggerUI webpage

--- DataModel/WorldCitiesContext.cs
+++ DataModel/WorldCitiesContext.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DataModel;
 
@@ -20,8 +21,15 @@ public partial class WorldCitiesContext : DbContext
     public virtual DbSet<Country> Countries { get; set; }
 
     protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see https://go.microsoft.com/fwlink/?LinkId=723263.
-        => optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=warrenWorldCities;Trusted_Connection=True;MultipleActiveResultSets=true");
+    {
+        if (optionsBuilder.IsConfigured)
+        {
+            return;
+        }
+        IConfigurationBuilder builder = new ConfigurationBuilder().AddJsonFile("appsettings.json", optional: false);
+        IConfigurationRoot configuration = builder.Build();
+        optionsBuilder.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+    }
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
--- MyWebAPI/Data/WorldCitiesCSV.cs
+++ MyWebAPI/Data/WorldCitiesCSV.cs
@@ -4,8 +4,8 @@
     {
         public string city { get; set; } = null!;
         public string city_ascii { get; set; } = null!;
-        public decimal lat { get; set; }
-        public decimal lng { get; set; }
+        public double lat { get; set; }
+        public double lng { get; set; }
         public string country { get; set; } = null!;
         public string iso2 { get; set; } = null!;
         public string iso3 { get; set; } = null!;
